c8384960d1372a53f3e1e63364b03601794cee1a
Isaac Vazquez <izzy@irunpy.net>
2018-10-26 20:32:03 -0700

    added blog and job models and registered admin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+	image = models.ImageField(upload_to='blogimages/')
+	title = models.CharField(max_length=100)
+	body = models.TextField()
+	time = models.DateTimeField(auto_created=True, auto_now=True)
+
+	def __str__(self):
+		return self.title
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'Izzy0215',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -120,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
