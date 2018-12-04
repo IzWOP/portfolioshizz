5351cfe5d2cd65da4a41ddd08885ae0cd42eb47a
Isaac Vazquez <izzy@irunpy.net>
2018-12-03 20:07:56 -0800

    fixed css problems header is viewable

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.index, name='index'),
     path('blog/', include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
