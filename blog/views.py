cfc36de0641a723a8ffa65e4dde93d6147bc15e6
Isaac Vazquez <izzy@irunpy.net>
2019-01-14 20:43:51 -0800

    updated active on navbar

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-
 from .models import Post
+
 def blogposts(request):
 	post = Post.objects
-	return render(request, 'blog/posts.html', {'post':post, 'title': 'Posts'})
+	return render(request, 'blog/posts.html', {'post':post, 'title': 'Posts', 'blog_page':'active'})
 
 def detail(request, blog_id):
 	detailblog = get_object_or_404(Post, pk=blog_id)
 	blogtitle = detailblog.title
 	return render(request, 'blog/detail.html',{'blog':detailblog,'title': blogtitle})
-
--- jobs/views.py
+++ jobs/views.py
@@ -4,4 +4,7 @@ from .models import Job
 
 def index(request):
 	jobs = Job.objects
-	return render(request, 'jobs/index.html', {'jobs': jobs})
+	return render(request, 'jobs/index.html', {'jobs': jobs, 'home_page':'active'})
+
+def about(request):
+	return render(request, 'jobs/about.html',{'about':'about','about_page':'active'})
