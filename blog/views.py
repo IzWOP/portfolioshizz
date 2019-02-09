061bd3b59fc7c71539024bfcfb572644e70e8f18
Isaac Vazquez <izzy@irunpy.net>
2019-02-08 21:55:20 -0800

    edited order of blog posts created scss files and compilers

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def blogposts(request):
-	post = Post.objects
+	post = Post.objects.all().order_by('-time')
 	return render(request, 'blog/posts.html', {'post':post, 'title': 'Posts', 'blog_page':'active'})
 
 def detail(request, blog_id):
